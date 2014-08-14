4a80cb5e5d969260082923927e41432ade082ad5
Axel Hecht <axel@pike.org>
2014-08-14 14:10:32 +0200

    bug 1043467, step 1: new directory structure Also, update version to 0.10.x, doing an intermediate release number
    --HG--
    rename : lib/Mozilla/__init__.py => compare_locales/__init__.py
    rename : lib/Mozilla/Checks.py => compare_locales/checks.py
    rename : lib/Mozilla/CompareLocales.py => compare_locales/compare.py
    rename : lib/Mozilla/Jars.py => compare_locales/jars.py
    rename : lib/Mozilla/Parser.py => compare_locales/parser.py
    rename : lib/Mozilla/Paths.py => compare_locales/paths.py
    rename : lib/Mozilla/tests/__init__.py => compare_locales/tests/__init__.py
    rename : lib/Mozilla/tests/test_checks.py => compare_locales/tests/test_checks.py
    rename : lib/Mozilla/tests/test_dtd.py => compare_locales/tests/test_dtd.py
    rename : lib/Mozilla/tests/test_ini.py => compare_locales/tests/test_ini.py
    rename : lib/Mozilla/tests/test_merge.py => compare_locales/tests/test_merge.py
    rename : lib/Mozilla/tests/test_properties.py => compare_locales/tests/test_properties.py
    rename : lib/Mozilla/tests/test_util.py => compare_locales/tests/test_util.py
    rename : lib/Mozilla/util.py => compare_locales/util.py
    extra : histedit_source : 46041c8e124e467377acb162a161b766b3f10f58%2Cf080fc5d383339bafc0f441a577d63a412bda51f

---
+++ tests/test_ini.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import re
+import unittest
+
+from compare_locales.parser import getParser
+
+class TestLineWraps(unittest.TestCase):
+
+    def setUp(self):
+	self.p = getParser('foo.ini')
+
+    def tearDown(self):
+	del self.p
+
+    def testSimpleHeader(self):
+	self.p.readContents('''; This file is in the UTF-8 encoding
+[Strings]
+TitleText=Some Title
+''')
+	for e in self.p:
+	    self.assertEqual(e.key, 'TitleText')
+	    self.assertEqual(e.val, 'Some Title')
+	self.assert_('UTF-8' in self.p.header)
+
+    def testMPL2_Space_UTF(self):
+	self.p.readContents('''; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+; You can obtain one at http://mozilla.org/MPL/2.0/.
+
+; This file is in the UTF-8 encoding
+[Strings]
+TitleText=Some Title
+''')
+	for e in self.p:
+	    self.assertEqual(e.key, 'TitleText')
+	    self.assertEqual(e.val, 'Some Title')
+	self.assert_('MPL' in self.p.header)
+
+    def testMPL2_Space(self):
+	self.p.readContents('''; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+; You can obtain one at http://mozilla.org/MPL/2.0/.
+
+[Strings]
+TitleText=Some Title
+''')
+	for e in self.p:
+	    self.assertEqual(e.key, 'TitleText')
+	    self.assertEqual(e.val, 'Some Title')
+	self.assert_('MPL' in self.p.header)
+
+    def testMPL2_MultiSpace(self):
+	self.p.readContents('''; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+; You can obtain one at http://mozilla.org/MPL/2.0/.
+
+; more comments
+
+[Strings]
+TitleText=Some Title
+''')
+	for e in self.p:
+	    self.assertEqual(e.key, 'TitleText')
+	    self.assertEqual(e.val, 'Some Title')
+	self.assert_('MPL' in self.p.header)
+
+    def testMPL2_Junk(self):
+	self.p.readContents('''; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+; You can obtain one at http://mozilla.org/MPL/2.0/.
+Junk
+[Strings]
+TitleText=Some Title
+''')
+        (junk_key, junk_val), (title_key, title_val) = \
+            ((e.key, e.val) for e in self.p)
+        self.assertTrue(re.match('_junk_\\d+_0-213$', junk_key))
+        self.assertEqual(junk_val, '''; This Source Code Form is subject to the terms of the Mozilla Public
+; License, v. 2.0. If a copy of the MPL was not distributed with this file,
+; You can obtain one at http://mozilla.org/MPL/2.0/.
+Junk
+[Strings]''')
+        self.assertEqual(title_key, 'TitleText')
+        self.assertEqual(title_val, 'Some Title')
+	self.assert_('MPL' not in self.p.header)
+
+if __name__ == '__main__':
+  unittest.main()
